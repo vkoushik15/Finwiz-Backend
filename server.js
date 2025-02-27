1088cff9b83d7aa87aa35f09cf78babca61d78b3
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-02-27 19:27:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.json());
 require("dotenv").config();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "http://localhost:5173", 
+    credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"],
-   
+    allowedHeaders: ["Content-Type", "Authorization"], 
 };
 app.use(cors(corsOptions));
 app.use("/user", userRoutes);

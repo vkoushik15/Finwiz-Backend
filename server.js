371dc27a65d877005200995c620130f58eac7746
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-02-27 20:09:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 require("dotenv").config();
 
 const corsOptions = {
-    origin: "http://localhost:5173", 
+    origin: "https://finwiz-c5772.web.app", 
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"], 
